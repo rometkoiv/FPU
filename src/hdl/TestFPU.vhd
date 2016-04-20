cafe299d453d9bb8f04c10b00fe24655f6a4ab8c
rometkoiv <rometkoiv@gmail.com>
2016-04-20 17:03:42 +0300

    Lisatud normaliseerimine ja parandatud liitmine ja lahutamine

--- src/hdl/TestFPU.vhd
+++ src/hdl/TestFPU.vhd
@@ -20,8 +20,8 @@ component float_calc is
          errorCode : out STD_LOGIC_VECTOR (3 downto 0);
          mantA_in : in STD_LOGIC_VECTOR (12 downto 0);
          mantB_in : in STD_LOGIC_VECTOR (12 downto 0);
-         powA : in STD_LOGIC_VECTOR (7 downto 0);
-         powB : in STD_LOGIC_VECTOR (7 downto 0);
+         powA_in : in STD_LOGIC_VECTOR (7 downto 0);
+         powB_in : in STD_LOGIC_VECTOR (7 downto 0);
          mode : in STD_LOGIC_VECTOR (1 downto 0);
          mant : out STD_LOGIC_VECTOR (12 downto 0);
          pow : out STD_LOGIC_VECTOR (7 downto 0));
@@ -37,15 +37,15 @@ test_float_calc: float_calc PORT MAP(
 
                     clk =>CLK,
                     errorCode=>error,
-                      --mantA_in => "0000000000101", --5
-                      mantA_in => "1010000100000",
+                      mantA_in => "0000000000101", --5
+                      --mantA_in => "1010000100000",
                       --mantA_in => "0111111111011", -- -5 
-                      mantB_in => "0000000000111", --7 
+                      mantB_in => "0000000001111", --7 
                       --powA => "10101111", -- -128
                       --powB => "10101111", -- -81
-                      powA => "00000001", -- 1
-                      powB => "00000001", -- 1
-                      mode => "10",
+                      powA_in => "00000010", -- 2
+                      powB_in => "00000001", -- 1
+                      mode => "01",
                       mant => mant,
                       pow =>pow
    );
--- src/hdl/float_calc.vhd
+++ src/hdl/float_calc.vhd
@@ -4,11 +4,8 @@ use IEEE.std_logic_signed.all;
 USE IEEE.NUMERIC_STD.ALL;
 
 --Veakoodid
---0001
---
 --1001 Positiivset tulemust ei saa vähendada
 --1010 Negatiivsed tulemust ei saa vähendada
---dd
 --1111 mantissi ei saa vähendada
 
 entity float_calc is
@@ -18,11 +15,12 @@ entity float_calc is
            errorCode : out STD_LOGIC_VECTOR (3 downto 0):=(others => '0');
            mantA_in : in STD_LOGIC_VECTOR (12 downto 0):=(others => '0');
            mantB_in : in STD_LOGIC_VECTOR (12 downto 0):=(others => '0');
-           powA : in STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
-           powB : in STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
+           powA_in : in STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
+           powB_in : in STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
            mode : in STD_LOGIC_VECTOR (1 downto 0);
            mant : out STD_LOGIC_VECTOR (12 downto 0):=(others => '0');
            pow : out STD_LOGIC_VECTOR (7 downto 0):=(others => '0'));
+           
 end float_calc;
 
 architecture Behavioral of float_calc is
@@ -33,10 +31,13 @@ begin
 float_calculator : process(clk)
 --variables
 variable tshift : INTEGER range 1 to (INTEGER'high):=1;
+variable powA : STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
+variable powB : STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
 variable powC : STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
 variable mantA : STD_LOGIC_VECTOR (13 downto 0):=(others => '0');
 variable mantB : STD_LOGIC_VECTOR (13 downto 0):=(others => '0');
 variable mantC : STD_LOGIC_VECTOR (13 downto 0):=(others => '0');
+variable mantT : STD_LOGIC_VECTOR (13 downto 0):=(others => '0');
 variable powMinus : STD_LOGIC_VECTOR (7 downto 0):=(others => '0');
 variable powPlus : STD_LOGIC_VECTOR (8 downto 0):=(others => '0');
 
@@ -44,22 +45,55 @@ variable minusOne : STD_LOGIC_VECTOR (13 downto 0):=(others => '1');
 variable mult : STD_LOGIC_VECTOR (27 downto 0):=(others => '0');
 variable multMax : INTEGER range 1 to (INTEGER'high);
 
+variable longShift : STD_LOGIC_VECTOR (27 downto 0):=(others => '0');
 
 begin
-  --Pikendame mantissi, et ületäitumine kaduma ei läheks
-  if mantA_in(12)='0' then
-    mantA:='0'&mantA_in;
+ powA:=powA_in;
+ powB:=powB_in;
+ --Pikendame mantisse, et hõlbustada liitmise ja lahutamise arvutamist
+ -- mantA:=mantA_in(mlen-1)&mantA_in;
+ -- mantB:=mantB_in(mlen-1)&mantB_in;
+  
+  --Normaliseerime
+  --A
+  if mantA_in(mlen-1)='1' then
+    for index in (mlen-2) downto 0 loop
+    if mantA_in(index)='0' then
+       mantA(mlen downto (mlen-2)-index):="11"&mantA_in(index downto 0);
+       powA:= powA-((mlen-2)-index);
+       exit;
+       end if;
+    end loop;
   else
-    mantA:='1'&mantA_in;
+    for index in (mlen-2) downto 0 loop
+      if mantA_in(index)='1' then
+         mantA(mlen downto (mlen-2)-index):="00"&mantA_in(index downto 0);
+         powA:=powA-((mlen-2)-index);
+         exit;
+         end if;
+      end loop;
   end if;
   
-  if mantB_in(12)='0' then
-      mantB:='0'&mantB_in;
-    else
-      mantB:='1'&mantB_in;
+    --B
+  if mantB(mlen-1)='1' then
+    for index in (mlen-2) downto 0 loop
+    if mantB_in(index)='0' then
+       mantB(mlen downto (mlen-2)-index):="11"&mantB_in(index downto 0);
+       powB:=powB-((mlen-2)-index);
+       exit;
+       end if;
+    end loop;
+  else
+    for index in (mlen-2) downto 0 loop
+      if mantB_in(index)='1' then
+         mantB(mlen downto (mlen-2)-index):="00"&mantB_in(index downto 0);
+         powB:=powB-((mlen-2)-index);
+         exit;
+         end if;
+      end loop;
   end if;
-  
-  
+ 
+   
   --Liitmine
    if mode="00" or mode="01" then
        
@@ -70,52 +104,34 @@ begin
         mantB := mult(13 downto 0);
       end if;
        
-      --Teeme kindlaks kumb mantiss on suurem
-      if (powA(plen)=powB(plen)) and (powB(plen) ='0') then
-      powC:=powA;
-      mant(plen)<='1';
-      for index in (plen-1) downto 0 loop
-          
-          if powB(index)> powA(index) then
-          
-          powC:=powB;
-          exit;
-          end if;   
-      end loop;
+      --Teeme kindlaks kumb astendaja on suurem
       
-      elsif (powA(plen)=powB(plen)) and (powB(plen) ='1') then
-      powC:=powB;
-      for index in (plen-1) downto 0 loop
-                if powB(index)> powA(index) then
-                powC:=powA;
-                exit;
-                end if;   
-      end loop;
-      
-      elsif powA(plen)>powB(plen) then
-      powC:=powB;
-      elsif powA(plen)<powB(plen) then
-            powC:=powA;
-      end if;
-      
-      if powC=powA then
-      
-      powMinus := powB-powA;
-      tshift := to_integer(unsigned(powMinus(6 downto 0)));
-      if powMinus(7)='1' then
-        mantC:=std_logic_vector(shift_left(unsigned(mantB), tshift))+mantA;
+      if to_integer(signed(powA)) > to_integer(signed(powB)) then
+         powC:=powA;
       else
-        mantC:=std_logic_vector(shift_right(unsigned(mantB), tshift))+mantA;
+         powC:=powB;
       end if;
+      
+      --Olenevalt kumb astendaja on suurem tuleb valem kirjutada
+      if powC=powA then
+       
+       powMinus := powB-powA;
+       errorCode<= powMinus(3 downto 0);
+       tshift := to_integer(signed(powMinus)*(-1));
+         if powMinus(plen)='1' then
+           mantC:=std_logic_vector(shift_right(signed(mantB), tshift))+mantA;
+         else
+           mantC:=std_logic_vector(shift_left(signed(mantB), tshift))+mantA;
+         end if;
       
       else
        powMinus := powA-powB;
-        tshift := to_integer(unsigned(powMinus(6 downto 0)));
-        if powMinus(7)='1' then
-           mantC:=std_logic_vector(shift_left(unsigned(mantA), tshift))+mantB;
-        else 
-           mantC:=std_logic_vector(shift_right(unsigned(mantA), tshift))+mantB;
-        end if;
+       tshift := to_integer(signed(powMinus)*(-1));
+       if powMinus(plen)='1' then
+          mantC:=std_logic_vector(shift_right(signed(mantA), tshift))+mantB;
+       else 
+          mantC:=std_logic_vector(shift_left(signed(mantA), tshift))+mantB;
+       end if;
       end if;
       
 
@@ -178,7 +194,7 @@ begin
    end if;
    
 
-      --kui mantiss on liiga suur suurendame astendajat
+      --kui mantiss on liiga suur, suurendame astendajat
       if to_integer(signed(mantC)) < -3072 or to_integer(signed(mantC)) > 4095 then
          if to_integer(signed(powC))<127 or to_integer(signed(powC))>-96 then
           if mantC(13)='0' then
